c155a4afd26e574e69e02f1915f53839793dc173
pavlovicIvan <ivanpavlo4@gmail.com>
2021-04-19 21:03:51 +0200

    Validation and styling update

--- src/components/Geolocator.js
+++ src/components/Geolocator.js
@@ -1,5 +1,6 @@
-// React
+// React, is-ip
 import React, { useState, useEffect } from "react";
+import isIp from "is-ip";
 
 // Custom functions
 import { callApi } from "../helpers/functions";
@@ -15,6 +16,7 @@ const Geolocator = () => {
   const [customIP, setCustopmIP] = useState("");
   const [result, setResult] = useState("");
   const [loading, setLoading] = useState("");
+  const [validation, setValidation] = useState(false);
 
   useEffect(() => {
     callApi(api, setLoading, setResult);
@@ -27,7 +29,12 @@ const Geolocator = () => {
   const handleSearch = (event) => {
     event.preventDefault();
 
-    callApi(`${api}${customIP}`, setLoading, setResult);
+    if (isIp(customIP)) {
+      callApi(`${api}${customIP}`, setLoading, setResult);
+      setValidation(false);
+    } else {
+      setValidation(true);
+    }
   };
 
   return (
@@ -40,6 +47,9 @@ const Geolocator = () => {
         />
         <input type="submit" value="SEARCH" />
       </form>
+      {validation && (
+        <div className="validationText"> IP address is not valid </div>
+      )}
       {loading ? (
         <>
           <div className="skeletonTable" />
